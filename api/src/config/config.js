ced218209a21e26a15207880f0638748b10f2c93
Frederico Brion <fredericoasbrion@gmail.com>
2024-03-04 15:27:01 -0300

    configurando db-5

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -3,8 +3,8 @@ const pg = require('pg');
 const config = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  host: process.env.POSTGRES_URL_NO_SSL,
+  database: process.env.POSTGRES_URL,
+  host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   dialectModule: pg
 };
